4194306f645a854ca2194e7527372570416baf61
constance <constance.mendez.harscouet@gmail.com>
2024-10-23 18:42:25 +0200

    refacto code (ajout de fonctions sup et reecriture des fonctions existantes

--- DB_test_sqlite3/test_sqlite3/script_test_db.py
+++ DB_test_sqlite3/test_sqlite3/script_test_db.py
@@ -1,26 +1,28 @@
 import sqlite3
 
-def create_table():
+def creer_table():
     
     connexion = sqlite3.connect("test.db")
 
     # objet curseur permet de faire les requêtes
     curseur = connexion.cursor()
 
-    # recréer la table à chaque lancement du script
-    curseur.execute("DROP TABLE IF EXISTS personnages_arcane")
-
-    curseur.execute('''CREATE TABLE IF NOT EXISTS 
+    query = '''CREATE TABLE IF NOT EXISTS 
     personnages_arcane(
     id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
     nom TEXT,
     genre TEXT,
     ville TEXT,
-    statut TEXT)''' )
+    statut TEXT)'''
+
+    # recréer la table à chaque lancement du script
+    curseur.execute("DROP TABLE IF EXISTS personnages_arcane")
+
+    curseur.execute(query)
 
     connexion.close()
 
-create_table()
+creer_table()
 
 ######################################
 
@@ -32,12 +34,14 @@ def inserer_perso(nom, genre, ville, statut):
 
     curseur = connexion.cursor()
 
-    curseur.execute('''
+    query = '''
     INSERT INTO personnages_arcane
     (nom, genre, ville, statut)
     VALUES
     ( ?, ?, ?, ?)
-    ''', (nom, genre, ville, statut))
+    '''
+    curseur.execute(query, (nom, genre, ville, statut))
+
     connexion.commit()
 
     connexion.close()
@@ -59,12 +63,13 @@ def inserer_plusieurs_persos(personnages_saison1):
 
     connexion = sqlite3.connect("test.db")
     curseur = connexion.cursor()
-    curseur.executemany('''
+    query = '''
     INSERT INTO personnages_arcane
     (nom, genre, ville, statut)
     VALUES 
     (?, ?, ?, ?)
-    ''', personnages_saison1)
+    '''
+    curseur.executemany(query, (personnages_saison1))
 
 # pas besoin d'indiquer l'ID, vu que celle-ci est auto-incrémentée et n'est pas présente dans
 # la liste des tuples au-dessus
@@ -78,35 +83,46 @@ inserer_plusieurs_persos(personnages_saison1)
 def recup_persos_info (id):
     connexion = sqlite3.connect("test.db")
     curseur = connexion.cursor()
-    res = curseur.execute("SELECT * FROM personnages_arcane WHERE id = ?", (id,))
+    query = '''
+    SELECT * FROM personnages_arcane WHERE id = ?
+    '''
+    res = curseur.execute(query, (id))
     perso = res.fetchall()
     connexion.close()
 
     return perso
 
-# print (recup_persos_info(7))
+#print (recup_persos_info(7))
 
 ######################################
 
-def mise_a_jour (statut):
+def mettre_a_jour_perso (nom_update, genre_update, ville_update, statut_update, perso_id):
     connexion = sqlite3.connect("test.db")
     curseur = connexion.cursor()
-    curseur.execute("UPDATE personnages_arcane SET statut = 'morte' WHERE statut = ?", (statut, ))
+    query = '''
+    UPDATE personnages_arcane 
+    SET nom=?, genre=?, ville=?, statut=?
+    WHERE id=?
+    '''
+    curseur.execute(query, (nom_update, genre_update, ville_update, statut_update, perso_id))
     connexion.commit()
     connexion.close()
 
-# mise_a_jour(statut = "inconnu")
+mettre_a_jour_perso("Vander", "homme", "Zaun", "mort", 11)
 
 ######################################
 
-def supprimer (id):
+def supprimer_perso (id):
     connexion = sqlite3.connect("test.db")
     curseur = connexion.cursor()
-    curseur.execute("DELETE FROM personnages_arcane WHERE id = ?", (id, ))
+    query = '''
+    DELETE FROM personnages_arcane WHERE id = ?
+    '''
+    curseur.execute(query, (id))
     connexion.commit()
     connexion.close()
 
-# supprimer (id = "9")
+supprimer_perso (id = "9")
 
 
 # requêtes d'interrogation - méthodes fetch
