3a83ebdcbaaea1afefe586b7f4e5abd54d4779ca
constance <constance.mendez.harscouet@gmail.com>
2024-11-05 18:23:03 +0100

    prb importation regle avec creation fichier init.py dans le repertoire source

--- Interface_test_PyQt/main_window.py
+++ Interface_test_PyQt/main_window.py
@@ -1,23 +1,12 @@
 import sys
-import os
+from ..DB_test_sqlite3 import script_test_db
+
 import sqlite3
 
 from PySide2.QtCore import Qt
 from PySide2.QtWidgets import (QApplication, QMainWindow, QVBoxLayout, QScrollArea, 
 QLineEdit, QHBoxLayout, QFrame, QPushButton, QLabel)
 
-'''
-chemin vers la BDD utilisant la fonction os.path.dirname pour trouver le répertoire qui contient la BDD et  
-la fonction os.path.join pour lier le chemin de l'interface au chemin de la bdd pour créer un chemin complet
-''' 
-# db_path = os.path.join(os.path.dirname(__file__), '..', 'DB_test_sqlite3', 'test.db')
-db_path = '/home/constance/Documents/ADA/PROJETS_PERSOS/projet_3DSMax/projet_3DSMax_code/DB_test_sqlite3/test.db'
-
-# ajout chemin contenant le fichier Python contenant mes fonctions liées à la BDD à la variable d'environnement
-# sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'DB_test_sqlite3'))
-sys.path.append('/home/constance/Documents/ADA/PROJETS_PERSOS/projet_3DSMax/projet_3DSMax_code/DB_test_sqlite3/script_test_db.py')
-
-import script_test_db
 
 class main_window(QMainWindow):
     
