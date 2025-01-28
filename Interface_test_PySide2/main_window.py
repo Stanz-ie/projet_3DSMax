4f704c5876d0a102e3f47671dd4d97a796b39fc7
constance <constance.mendez.harscouet@gmail.com>
2025-01-28 12:41:17 +0100

    nom dossier modifie + test avec les widgets

--- Interface_test_PySide2/main_window.py
+++ Interface_test_PySide2/main_window.py
@@ -3,11 +3,13 @@ sys.path.append('/home/constance/Documents/ADA/PROJETS_PERSOS/projet_3DSMax/proj
 from DB_test_sqlite3.script_test_db import create_table, insert_shot, insert_all_shots, update_shot, delete_shot
 
 from PySide2.QtCore import Qt
-from PySide2.QtWidgets import (QApplication, QMainWindow, QVBoxLayout, QScrollArea, 
+from PySide2.QtWidgets import (QApplication, QMainWindow, QWidget, QVBoxLayout, QScrollArea, 
 QLineEdit, QDateEdit, QHBoxLayout, QFrame, QPushButton, QLabel)
+from PySide2.QtGui import QPalette, QColor
 
 from datetime import datetime
 
+'''
 class CreateRecord(QFrame):
     def __init__(self, main_window):
         super().__init__()
@@ -39,28 +41,44 @@ class CreateRecord(QFrame):
             self.shot_name.clear()  
             self.path.clear()
 
+class Color(QWidget):
+
+    def __init__(self, color):
+        super().__init__()
+        self.setAutoFillBackground(True)
+
+        palette = self.palette()
+        palette.setColor(QPalette.Window, QColor(color))
+        self.setPalette(palette)
+'''
 class MainWindow(QMainWindow):
     
     def __init__(self):
         # constructeur parent (QMainWindow)
         super().__init__()
+       
         self.setWindowTitle("Mon Interface")
-        self.resize(600, 500)
-        create_table()
-    
-    def initUI(self):
-        self.main_frame = QFrame()
-        self.main_layout = QVBoxLayout(self.main_frame)
-    # Create an instance of CreateRecord
-    # Pass a reference to the main window
-        self.register_widget = CreateRecord(self)
-        self.main_layout.addWidget(self.register_widget)
-        self.setCentralWidget(self.main_frame)
-  
-    
+        self.setFixedSize(700, 600)
+
+        '''
+       layout = QVBoxLayout()
+
+        layout.addWidget(Color('yellow'))
+        layout.addWidget(Color('green'))
+        layout.addWidget(Color('red'))
+        layout.addWidget(Color('purple'))
+        layout.addWidget(Color('blue'))
+
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+        '''
+ 
+        #create_table()
+
 def main():
+    
     # On crée l'instance d'application
-    # possible de créer l'instance sans liste d'arguments 
     # si on sait qu'on n'utilisera pas le terminal pour controler QT
     # dans ce cas, juste créer une liste vide app = QApplication([])
     app = QApplication([])  
@@ -70,10 +88,22 @@ def main():
     win.show()
     # On démarre la boucle de gestion des événements. 
     app.exec_()
-    
 
-if __name__ == "__main__":
-    main()    
+if __name__ == '__main__':
+    main()
+
+#Color('red')
+     
+'''
+    def initUI(self):
+        self.main_frame = QFrame()
+        self.main_layout = QVBoxLayout(self.main_frame)
+    # Create an instance of CreateRecord
+    # Pass a reference to the main window
+        self.register_widget = CreateRecord(self)
+        self.main_layout.addWidget(self.register_widget)
+        self.setCentralWidget(self.main_frame)
+'''    
 
 
     
